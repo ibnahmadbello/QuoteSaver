eabc3547057ea0e1de4645f1f823df8598f5b424
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-06-07 18:03:08 +0100

    Design dependency

--- app/src/main/java/com/regent/tech/quote_saver/database/DatabaseHelper.java
+++ app/src/main/java/com/regent/tech/quote_saver/database/DatabaseHelper.java
@@ -1,4 +1,30 @@
 package com.regent.tech.quote_saver.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.regent.tech.quote_saver.database.model.Quote;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "quotes.db";
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(Quote.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Quote.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 }
