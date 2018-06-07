4fd9d530231d6b12432256c1fae53302fa39b89a
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-06-08 00:00:36 +0100

    Database Helper Class

--- app/src/main/java/com/regent/tech/quote_saver/database/DatabaseHelper.java
+++ app/src/main/java/com/regent/tech/quote_saver/database/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.regent.tech.quote_saver.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -26,5 +28,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Quote.TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    public long insertQuote(String quote, String quote_author, String quote_source){
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Quote.COLUMN_QUOTE, quote);
+        values.put(Quote.COLUMN_QUOTE_AUTHOR, quote_author);
+        values.put(Quote.COLUMN_QUOTE_SOURCE, quote_source);
+
+        long id = database.insert(Quote.TABLE_NAME, null, values);
+
+        database.close();
+
+        return id;
+    }
+
+    public Quote getQuote(long id){
+        SQLiteDatabase database = this.getReadableDatabase();
+
+        Cursor cursor = database.query(Quote.TABLE_NAME, new String[]{Quote.COLUMN_QUOTE_AUTHOR,
+                Quote.COLUMN_QUOTE, Quote.COLUMN_TIMESTAMP}, Quote.COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null){
+            cursor.moveToFirst();
+        }
+
+        Quote quote = new Quote(
+                cursor.getInt(cursor.getColumnIndex(Quote.COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(Quote.COLUMN_QUOTE)),
+                cursor.getString(cursor.getColumnIndex(Quote.COLUMN_QUOTE_AUTHOR)),
+                cursor.getString(cursor.getColumnIndex(Quote.COLUMN_QUOTE_SOURCE)),
+                cursor.getString(cursor.getColumnIndex(Quote.COLUMN_TIMESTAMP))
+        );
+
+        cursor.close();
+
+        return quote;
     }
 }
--- app/src/main/java/com/regent/tech/quote_saver/view/QuotesAdapter.java
+++ app/src/main/java/com/regent/tech/quote_saver/view/QuotesAdapter.java
@@ -1,4 +1,34 @@
 package com.regent.tech.quote_saver.view;
 
-public class QuotesAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.regent.tech.quote_saver.database.model.Quote;
+
+import java.util.List;
+
+public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<Quote> quoteList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView quote;
+        public TextView quote_author;
+        public TextView dot;
+        public TextView timestamp;
+
+        public MyViewHolder(View view){
+            super(view);
+        }
+
+    }
+
+    public QuotesAdapter(Context context, List<Quote> quoteList){
+        this.context = context;
+        this.quoteList = quoteList;
+    }
+
 }
