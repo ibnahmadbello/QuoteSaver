3a48fff96ff00375c40a6589f3e9c58dee451534
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-06-07 15:03:20 +0100

    Created three packages (view, util and database)

--- app/src/main/java/com/regent/tech/quote_saver/view/MainActivity.java
+++ app/src/main/java/com/regent/tech/quote_saver/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.regent.tech.quote_saver;
+package com.regent.tech.quote_saver.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.regent.tech.quote_saver.R;
 
 public class MainActivity extends AppCompatActivity {
 
